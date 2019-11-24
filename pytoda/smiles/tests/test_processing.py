00b087a1032e45cf8477913309921b727d5474de
Jannis Born <jab@zurich.ibm.com>
2019-11-24 23:08:10 +0100

    doc: added unittests for new functions

--- pytoda/smiles/tests/test_processing.py
+++ pytoda/smiles/tests/test_processing.py
@@ -1,8 +1,9 @@
 """Testing SMILES processing."""
 import unittest
 from pytoda.smiles.processing import (
-    apply_normalization_dictionary, tokenize_smiles
+    apply_normalization_dictionary, tokenize_smiles, tokenize_selfies
 )
+from pytoda.smiles.transforms import Selfies
 
 
 class TestProcessing(unittest.TestCase):
@@ -37,6 +38,31 @@ class TestProcessing(unittest.TestCase):
             self.assertListEqual(
                 tokenize_smiles(smiles, normalize=True), ground_truth
             )
+
+    def test_tokenize_selfies(self) -> None:
+        """Test tokenize_selfies."""
+        for smiles, ground_truth in [
+            (
+                'c1cnoc1',
+                ['[c]', '[c]', '[n]', '[o]', '[c]', '[Ring1]', '[Ring2]']
+            ),
+            (
+                '[O-][n+]1ccccc1S', [
+                    '[O-expl]', '[n+expl]', '[c]', '[c]', '[c]', '[c]', '[c]',
+                    '[Ring1]', '[Branch1_1]', '[S]'
+                ]
+            ),
+            (
+                'c1snnc1-c1ccccn1', [
+                    '[c]', '[s]', '[n]', '[n]', '[c]', '[Ring1]', '[Ring2]',
+                    '[-c]', '[c]', '[c]', '[c]', '[c]', '[n]', '[Ring1]',
+                    '[Branch1_1]'
+                ]
+            )
+        ]:
+            transform = Selfies()
+            selfies = transform(smiles)
+            self.assertListEqual(tokenize_selfies(selfies), ground_truth)
 
 
 if __name__ == '__main__':
