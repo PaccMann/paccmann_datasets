93af143dec1858da7089f368b99def166d8063ed
Jannis Born <jab@zurich.ibm.com>
2020-03-06 11:42:03 +0100

    test: added smiles parameter testing for polymerdataset

--- pytoda/datasets/tests/test_polymer_dataset.py
+++ pytoda/datasets/tests/test_polymer_dataset.py
@@ -49,6 +49,102 @@ class TestPolymerDataset(unittest.TestCase):
 
                     self.assertEqual(len(polymer_dataset), 3)
 
+    def test_smiles_params(self) -> None:
+
+        content_monomer = os.linesep.join(
+            [
+                'CCO	CHEMBL545',
+                'C	CHEMBL17564',
+                'CO	CHEMBL14688',
+                'NCCS	CHEMBL602',
+            ]
+        )
+        content_catalyst = os.linesep.join(
+            [
+                'N#CCCC1CCCC1=NNc1ccc([N+](=O)[O-])cc1	CHEMBL543',
+                'CC	CHEMBL17',
+                'NCCSCCCCC	CHEMBL6402',
+            ]
+        )
+
+        annotated_content = os.linesep.join(
+            [
+                'label_0,label_1,monomer,catalyst',
+                '2.3,3.4,CHEMBL545,CHEMBL17',
+                '4.5,5.6,CHEMBL17564,CHEMBL6402',  # yapf: disable
+                '6.7,7.8,CHEMBL602,CHEMBL6402'
+            ]
+        )
+
+        with TestFileContent(content_monomer) as a_test_file:
+            with TestFileContent(content_catalyst) as another_test_file:
+                with TestFileContent(annotated_content) as annotation_file:
+                    polymer_dataset = PolymerDataset(
+                        smi_filepaths=[
+                            a_test_file.filename, another_test_file.filename
+                        ],
+                        annotations_filepath=annotation_file.filename,
+                        entity_names=['monomer', 'cATalysT'],
+                        all_bonds_explicit=True,
+                        all_hs_explicit=[True, False]
+                    )
+
+                    pad_ind = polymer_dataset.smiles_language.padding_index
+                    monomer_start_ind = (
+                        polymer_dataset.smiles_language.
+                        token_to_index['<MONOMER_START>']
+                    )
+                    monomer_stop_ind = (
+                        polymer_dataset.smiles_language.
+                        token_to_index['<MONOMER_STOP>']
+                    )
+                    catalyst_start_ind = (
+                        polymer_dataset.smiles_language.
+                        token_to_index['<CATALYST_START>']
+                    )
+                    catalyst_stop_ind = (
+                        polymer_dataset.smiles_language.
+                        token_to_index['<CATALYST_STOP>']
+                    )
+                    ch3_ind = polymer_dataset.smiles_language.token_to_index[
+                        '[CH3]']
+                    oh_ind = polymer_dataset.smiles_language.token_to_index[
+                        '[OH]']
+                    ch2_ind = polymer_dataset.smiles_language.token_to_index[
+                        '[CH2]']
+                    b_ind = polymer_dataset.smiles_language.token_to_index['-']
+                    c_ind = polymer_dataset.smiles_language.token_to_index['C']
+
+                    # test first sample
+                    monomer, catalyst, labels = polymer_dataset[0]
+
+                    self.assertEqual(
+                        monomer.numpy().flatten().tolist(), [
+                            pad_ind, pad_ind, monomer_start_ind, ch3_ind,
+                            b_ind, ch2_ind, b_ind, oh_ind, monomer_stop_ind
+                        ]
+                    )
+                    self.assertEqual(
+                        catalyst.numpy().flatten().tolist(), [
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, pad_ind, pad_ind, pad_ind, pad_ind,
+                            pad_ind, catalyst_start_ind, c_ind, b_ind, c_ind,
+                            catalyst_stop_ind
+                        ]
+                    )
+                    self.assertTrue(
+                        np.allclose(
+                            labels.numpy().flatten().tolist(), [2.3, 3.4]
+                        )
+                    )
+
     def test___getitem__(self) -> None:
         """Test __getitem__."""
         content_monomer = os.linesep.join(
