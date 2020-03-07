7f54c9d66f5c3df904a0f39f3cefc7ef1b40ee76
Jannis Born <jab@zurich.ibm.com>
2020-03-07 23:05:03 +0100

    feat: generalize _smi_eager_dataset for arbitrary tab-separated input files

--- pytoda/datasets/_smi_eager_dataset.py
+++ pytoda/datasets/_smi_eager_dataset.py
@@ -11,19 +11,21 @@ class _SmiEagerDataset(Dataset):
     In case of out of memory errors consider using _SmiLazyDataset.
     """
 
-    def __init__(self, smi_filepath: str) -> None:
+    def __init__(self, smi_filepath: str, name: str = 'SMILES') -> None:
         """
         Initialize a .smi dataset.
 
         Args:
             smi_filepath (str): path to .smi file.
+            name (str): type of dataset, used to index columns.
         """
         Dataset.__init__(self)
         self.smi_filepath = smi_filepath
-        self.smiles_df = read_smi(self.smi_filepath)
+        self.name = name
+        self.df = read_smi(self.smi_filepath, names=[self.name])
         self.sample_to_index_mapping = {
             sample: index
-            for index, sample in enumerate(self.smiles_df.index.tolist())
+            for index, sample in enumerate(self.df.index.tolist())
         }
         self.index_to_sample_mapping = {
             index: sample
@@ -32,7 +34,7 @@ class _SmiEagerDataset(Dataset):
 
     def __len__(self) -> int:
         """Total number of samples."""
-        return self.smiles_df.shape[0]
+        return self.df.shape[0]
 
     def __getitem__(self, index: int) -> str:
         """
@@ -45,4 +47,4 @@ class _SmiEagerDataset(Dataset):
             torch.tensor: a torch tensor of token indexes,
                 for the current sample.
         """
-        return self.smiles_df.iloc[index]['SMILES']
+        return self.df.iloc[index][self.name]
