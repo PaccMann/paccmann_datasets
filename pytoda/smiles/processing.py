53149b69446ec8ee2380e9c0eef4a152cae0ce1a
Jannis Born <jab@zurich.ibm.com>
2020-06-11 13:22:27 +0200

    feat: Add kmer tokenizer from SMILES-PE

--- pytoda/smiles/processing.py
+++ pytoda/smiles/processing.py
@@ -2,6 +2,8 @@
 import logging
 import re
 
+from SmilesPE.pretokenizer import kmer_tokenizer
+
 from ..types import Tokens
 
 # tokenizer
@@ -9,21 +11,15 @@ SMILES_TOKENIZER = re.compile(
     r'(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|'
     r'-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])'
 )
-# handle "-" character
-SMILES_NORMALIZER = re.compile(r'-(\w)')
-
 logger = logging.getLogger(__name__)
 
 
-def tokenize_smiles(smiles: str, normalize=False, regexp=None) -> Tokens:
+def tokenize_smiles(smiles: str, regexp=None) -> Tokens:
     """
     Tokenize a character-level SMILES string.
 
     Args:
         smiles (str): a SMILES representation.
-        normalize (bool): whether normalization is done.
-            NOTE: This argument is deprecated and will be removed in a future
-            release.
         regexp (None, re.Pattern): optionally pass a regexp for the
             tokenization. If none is passed, SMILES_TOKENIZER is used.
     Returns:
@@ -31,6 +27,26 @@ def tokenize_smiles(smiles: str, normalize=False, regexp=None) -> Tokens:
     """
     smiles_tokenizer = SMILES_TOKENIZER if regexp is None else regexp
     return [token for token in smiles_tokenizer.split(smiles) if token]
+
+
+def kmer_smiles_tokenizer(
+    smiles: str, k: int, stride: int = 1, *args, **kwargs
+) -> Tokens:
+    """K-Mer SMILES tokenization following SMILES PE (Li et al. 2020)
+
+    Args:
+        smiles (str): SMILES string to be tokenized.
+        k (int): Positive integer denoting the tuple/k-gram lengths.
+        stride (int, optional): Stride used for k-mer generation. Higher values
+            result in less tokens. Defaults to 1 (densely overlapping).
+        args (): Optional arguments for `kmer_tokenizer`.
+        kwargs (): Optional keyword arguments for `kmer_tokenizer`.
+
+    Returns:
+        Tokens: Tokenized SMILES sequence (list of str).
+    """
+
+    return kmer_tokenizer(smiles, ngram=k, stride=stride, *args, **kwargs)
 
 
 def tokenize_selfies(selfies: str) -> Tokens:
