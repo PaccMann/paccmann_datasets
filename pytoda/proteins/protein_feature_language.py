4d42b72f72392bdd1a72723d064d50fa93cf1d3a
Anna Weber <wbr@zurich.ibm.com>
2020-07-23 14:39:10 +0200

    feat: enable normalized BLOSUM encoding

--- pytoda/proteins/protein_feature_language.py
+++ pytoda/proteins/protein_feature_language.py
@@ -1,5 +1,5 @@
 """Protein language handling."""
-from .processing import AA_PROPERTIES_NUM, AA_FEAT, BLOSUM62
+from .processing import AA_PROPERTIES_NUM, AA_FEAT, BLOSUM62, BLOSUM62_norm
 from .protein_language import ProteinLanguage
 
 
@@ -39,6 +39,8 @@ class ProteinFeatureLanguage(ProteinLanguage):
             self.token_to_index = AA_FEAT
         elif self.feat == 'blosum':
             self.token_to_index = BLOSUM62
+        elif self.feat == 'blosum_norm':
+            self.token_to_index = BLOSUM62_norm
         else:
             raise ValueError(
                 "Choose dict as 'binary_features', 'float_features' or "
