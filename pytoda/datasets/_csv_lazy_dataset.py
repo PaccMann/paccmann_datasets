feat: correct feature wise statistics ignoring NaN, optional float imputation after scaling.

pytoda/datasets/_csv_lazy_dataset.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import numpy as np
 import pandas as pd
 
-from ..types import CallableOnSource, FeatureList, Hashable
+from ..types import CallableOnSource, FeatureList, Hashable, Optional, Union
 from ._cache_datasource import _CacheDatasource
 from ._csv_statistics import _CsvStatistics
 from .base_dataset import KeyDataset
@@ -66,12 +66,17 @@ class _CsvLazyDataset(KeyDataset, _CacheDatasource, _CsvStatistics):
         self.key_to_index_mapping = {}
         index = 0
         self.ordered_keys = []
+        # look ahead for row length
+        chunk = next(pd.read_csv(self.filepath, chunksize=1, **self.kwargs))
+        self.notna_count = np.array([0] * self.preprocess_df(chunk).shape[1])
+
         for chunk in pd.read_csv(
             self.filepath, chunksize=self.chunk_size, **self.kwargs
         ):
             chunk = self.preprocess_df(chunk)
             self.min_max_scaler.partial_fit(chunk.values)
             self.standardizer.partial_fit(chunk.values)
+            self.notna_count += chunk.notna().sum().values
             for key, row in chunk.iterrows():
                 self.cache[index] = row.values
                 self.key_to_index_mapping[key] = index
@@ -109,16 +114,24 @@ class _CsvLazyDataset(KeyDataset, _CacheDatasource, _CsvStatistics):
         return feature_fn
 
     def transform_datasource(
-            self, transform_fn: CallableOnSource, feature_fn: CallableOnSource
-    ):
+        self, transform_fn: CallableOnSource, feature_fn: CallableOnSource,
+        impute: Optional[float] = None
+    ) -> None:
         """Apply scaling to the datasource.
 
         Args:
             transform_fn (CallableOnSource): transformation on source data.
             feature_fn (CallableOnSource): function that indexes datasource.
+            impute (Optional[float]): NaN imputation with value if
+                given. Defaults to None.
         """
-        for index in self.cache:
-            self.cache[index] = transform_fn(feature_fn(self.cache[index]))
+        if impute is None:
+            for index in self.cache:
+                self.cache[index] = transform_fn(feature_fn(self.cache[index]))
+        else:
+            for index in self.cache:
+                transformed = transform_fn(feature_fn(self.cache[index]))
+                self.cache[index] = np.nan_to_num(transformed, impute)
 
     def __len__(self) -> int:
         """Total number of samples."""

pytoda/datasets/_csv_statistics.py
@@ -1,13 +1,13 @@
 """Abstract implementation of _CsvStatistics."""
 import copy
 from functools import reduce
-from typing import OrderedDict
 
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-from ..types import CallableOnSource, FeatureList, List, Tuple
+from ..types import (CallableOnSource, FeatureList, List, Optional,
+                     OrderedDict, Tuple, Union)
 
 
 class _CsvStatistics:
@@ -45,6 +45,7 @@ class _CsvStatistics:
         )
         self.setup_datasource()
 
+        # self.notna_count
         self.max = self.min_max_scaler.data_max_
         self.min = self.min_max_scaler.data_min_
         self.mean = self.standardizer.mean_
@@ -94,18 +95,26 @@ class _CsvStatistics:
         raise NotImplementedError
 
     def transform_datasource(
-        self, transform_fn: CallableOnSource, feature_fn: CallableOnSource
+        self,
+        transform_fn: CallableOnSource,
+        feature_fn: CallableOnSource,
+        impute: Optional[float] = None
     ) -> None:
         """Apply scaling to the datasource.
 
         Args:
             transform_fn (CallableOnSource): transformation on source data.
             feature_fn (CallableOnSource): function that indexes datasource.
+            impute (Optional[float]): NaN imputation with value if
+                given. Defaults to None.
         """
         raise NotImplementedError
 
     def transform_dataset(
-        self, transform_fn: CallableOnSource, feature_list: FeatureList
+        self,
+        transform_fn: CallableOnSource,
+        feature_list: FeatureList,
+        impute: Optional[float] = None
     ) -> None:
         """Apply filtering, ordering and scaling to the datasource and update
         the dataset accordingly.
@@ -114,6 +123,8 @@ class _CsvStatistics:
             transform_fn (CallableOnSource): data scaling function, possibly
                 feature wise.
             feature_list (FeatureList): subset of features to return in order.
+            impute (Optional[float]): NaN imputation with value if
+                given. Defaults to None.
 
         Sets:
         feature_list (FeatureList): feature names in this datasource.
@@ -125,12 +136,13 @@ class _CsvStatistics:
         feature_fn = self.get_feature_fn(feature_list)
         statistics_indices = self.feature_mapping[feature_list].values
         # update datasource
-        self.transform_datasource(transform_fn, feature_fn)
+        self.transform_datasource(transform_fn, feature_fn, impute)
         # update statistics ordering (this could allow inverse transformation)
         self.max = self.max[statistics_indices]
         self.min = self.min[statistics_indices]
         self.mean = self.mean[statistics_indices]
         self.std = self.std[statistics_indices]
+        self.notna_count = self.notna_count[statistics_indices]
         # delete outdated scalers
         try:
             del self.min_max_scaler
@@ -209,31 +221,33 @@ def reduce_csv_statistics(
                 csv_dataset.min[csv_dataset.feature_mapping[features].values],
                 csv_dataset.mean[csv_dataset.feature_mapping[features].values],
                 csv_dataset.std[csv_dataset.feature_mapping[features].values],
-                len(csv_dataset)
+                # len(csv_dataset)
+                csv_dataset.notna_count[
+                    csv_dataset.feature_mapping[features].values]
             ) for csv_dataset in csv_datasets
         ]
     )
     # NOTE: reduce max and min
-    maximum = np.array(maximums).max(axis=0)
-    minimum = np.array(minimums).min(axis=0)
+    maximum = np.nanmax(maximums, axis=0)
+    minimum = np.nanmin(minimums, axis=0)
     # NOTE: reduce the mean
-    total_number_of_samples = float(sum(sample_numbers))
-    mean = np.array(
+    total_number_of_samples = sum(sample_numbers).astype(float)
+    mean = np.nansum(
         [
             dataset_mean * number_of_samples
             for dataset_mean, number_of_samples in zip(means, sample_numbers)
-        ]
-    ).sum(axis=0) / total_number_of_samples
+        ],
+        axis=0
+    ) / total_number_of_samples
     # NOTE: reduce the std
     std = np.sqrt(
-        (
-            np.array(
-                [
-                    (dataset_std**2 + dataset_mean**2) * number_of_samples
-                    for dataset_std, dataset_mean, number_of_samples in
-                    zip(stds, means, sample_numbers)
-                ]
-            ).sum(axis=0) / total_number_of_samples - mean**2
-        )
+        np.nansum(
+            [
+                (dataset_std**2 + dataset_mean**2) * number_of_samples
+                for dataset_std, dataset_mean, number_of_samples in
+                zip(stds, means, sample_numbers)
+            ],
+            axis=0
+        ) / total_number_of_samples - mean**2
     )
     return (features, maximum, minimum, mean, std)

pytoda/datasets/_table_dataset.py
@@ -5,7 +5,7 @@ from functools import partial
 import numpy as np
 import torch
 
-from ..types import FeatureList, Files, Tensor, CsvSourceData
+from ..types import FeatureList, Files, Tensor, CsvSourceData, Optional, Union
 from ._csv_eager_dataset import _CsvEagerDataset
 from ._csv_lazy_dataset import _CsvLazyDataset
 from ._csv_statistics import reduce_csv_statistics
@@ -65,6 +65,7 @@ class _TableDataset(DatasetDelegator):
         standardize: bool = True,
         min_max: bool = False,
         processing_parameters: dict = {},
+        impute: Optional[float] = None,
         dtype: torch.dtype = torch.float,
         device: torch.device = torch.
         device('cuda' if torch.cuda.is_available() else 'cpu'),
@@ -85,6 +86,8 @@ class _TableDataset(DatasetDelegator):
                 required order and subset of features has to match that
                 determined by the dataset setup (see `self.feature_list` after
                 initialization). Defaults to {}.
+            impute (Optional[float]): NaN imputation with value if
+                given. Defaults to None.
             dtype (torch.dtype): data type. Defaults to torch.float.
             device (torch.device): device where the tensors are stored.
                 Defaults to gpu, if available.
@@ -159,10 +162,11 @@ class _TableDataset(DatasetDelegator):
                         'max': maximum.tolist()
                     }
             }
-        # Filter, order and transform the datasets
+        # Filter, order and transform the datasets and impute missing values
         for dataset in self.dataset.datasets:
             dataset.transform_dataset(
-                transform_fn=self.transform_fn, feature_list=self.feature_list
+                transform_fn=self.transform_fn, feature_list=self.feature_list,
+                impute=impute
             )
 
     def _setup_dataset(self) -> None:

pytoda/datasets/gene_expression_dataset.py
@@ -2,7 +2,7 @@
 import torch
 from .base_dataset import DatasetDelegator
 from ._table_dataset import _TableEagerDataset, _TableLazyDataset
-from ..types import Files, GeneList
+from ..types import Files, GeneList, Optional
 
 TABLE_DATASET_IMPLEMENTATIONS = {
     'eager': _TableEagerDataset,
@@ -22,6 +22,7 @@ class GeneExpressionDataset(DatasetDelegator):
         standardize: bool = True,
         min_max: bool = False,
         processing_parameters: dict = {},
+        impute: Optional[float] = 0.0,
         dtype: torch.dtype = torch.float,
         device: torch.device = torch.
         device('cuda' if torch.cuda.is_available() else 'cpu'),
@@ -45,6 +46,8 @@ class GeneExpressionDataset(DatasetDelegator):
                 required order and subset of features has to match that
                 determined by the dataset setup (see `self.gene_list` after
                 initialization). Defaults to {}.
+            impute (Optional[float]): NaN imputation with value if
+                given. Defaults to 0.0.
             dtype (torch.dtype): data type. Defaults to torch.float.
             device (torch.device): device where the tensors are stored.
                 Defaults to gpu, if available.
@@ -66,6 +69,7 @@ class GeneExpressionDataset(DatasetDelegator):
             standardize=standardize,
             min_max=min_max,
             processing_parameters=processing_parameters,
+            impute=impute,
             dtype=dtype,
             device=device,
             chunk_size=chunk_size,

pytoda/datasets/tests/test_gene_expression_dataset.py
@@ -109,7 +109,7 @@ class TestGeneExpressionDatasetEagerBackend(unittest.TestCase):
             TestFileContent(self.other_content) as another_test_file \
         :
             # feature not in data is filled with zeros
-            feature_list = ['C', 'D', 'B', 'E', 'all_missing']
+            feature_list = ['E', 'C', 'D', 'B', 'all_missing']
             standard_dataset = GeneExpressionDataset(
                 a_test_file.filename,
                 another_test_file.filename,
@@ -117,7 +117,7 @@ class TestGeneExpressionDatasetEagerBackend(unittest.TestCase):
                 backend=self.backend,
                 index_col=0
             )
-            # NOTE: numpy and pytoda use ddof of 0, whereas pandas default is 1
+            self.assertEqual(standard_dataset[0][-1], 0)
 
             gene_list = standard_dataset.gene_list
             df = pd.concat(
@@ -152,6 +152,7 @@ class TestGeneExpressionDatasetEagerBackend(unittest.TestCase):
                     standardize=True,
                     min_max=False,
                     processing_parameters=processing_parameters,
+                    impute=None,
                 )
 
                 # collect flat values
@@ -162,6 +163,7 @@ class TestGeneExpressionDatasetEagerBackend(unittest.TestCase):
 
             mean_array = df.mean().values
             std_array = df.std(ddof=0).values
+            # NOTE: numpy and pytoda use ddof of 0, whereas pandas default is 1
             for mean, std in [
                 # list
                 [mean_array.tolist(), std_array.tolist()],
@@ -181,6 +183,7 @@ class TestGeneExpressionDatasetEagerBackend(unittest.TestCase):
                     standardize=True,
                     min_max=False,
                     processing_parameters=processing_parameters,
+                    impute=None,
                 )
 
                 # collect transformed values
@@ -190,9 +193,10 @@ class TestGeneExpressionDatasetEagerBackend(unittest.TestCase):
                 ds_stds = np.nanstd(ds_2d, axis=0)
 
                 # debug std
-                standard_ds.std
-                std_array        # == df.std(ddof=0).values
-                df.std().values  # == df.std(ddof=1).values
+                # standard_ds.std
+                # std_array        # == df.std(ddof=0).values
+                # df.std().values  # == df.std(ddof=1).values
+
                 for index, feature in enumerate(standard_ds.gene_list):
                     if feature == 'all_missing':
                         # no features at all so transformed stat is nan

pytoda/types.py
@@ -11,7 +11,9 @@ from typing import (
     Mapping,
     Sequence,
     Tuple,
-    Union
+    Union,
+    Optional,
+    OrderedDict
 )
 
 from numpy import ndarray  # np.typing in development