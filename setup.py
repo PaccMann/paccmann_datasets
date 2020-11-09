0d8d93284d2e0651e3129041a738cd99f16fd8b8
Nikita Janakarajan <NJA@zurich.ibm.com>
2020-11-09 15:53:57 +0100

    dependency added : neuralnet-pytorch

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'SmilesPE>=0.0.3',
         'pyfaidx',
         'pubchempy',
+        'neuralnet-pytorch',
     ],
     classifiers=[
         'Intended Audience :: Developers',
@@ -63,12 +64,13 @@ setup(
     ],
     packages=find_packages(),
     package_data={
-        'pytoda.smiles.metadata': [
-            'spe_chembl.txt',
-            'ATTRIBUTION',
-            'README.md',
-            'tokenizer_chembl_gdsc_ccle_tox21_zinc_organdb_bindingdb/*',
-        ]
+        'pytoda.smiles.metadata':
+            [
+                'spe_chembl.txt',
+                'ATTRIBUTION',
+                'README.md',
+                'tokenizer_chembl_gdsc_ccle_tox21_zinc_organdb_bindingdb/*',
+            ]
     },
     scripts=scripts,
 )
