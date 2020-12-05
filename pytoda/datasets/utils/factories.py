9cfe3afef737dd38a4c80911d8f9d72450021139
Nikita Janakarajan <NJA@zurich.ibm.com>
2020-12-05 23:48:34 +0100

    chore: removed background tensor factory

--- pytoda/datasets/utils/factories.py
+++ pytoda/datasets/utils/factories.py
@@ -1,9 +1,5 @@
 import torch
-from pytoda.datasets.utils.wrappers import (
-    WrapperCDist,
-    WrapperKLDiv,
-    WrapperBackgroundTensor,
-)
+from pytoda.datasets.utils.wrappers import WrapperCDist, WrapperKLDiv
 
 METRIC_FUNCTION_FACTORY = {'p-norm': WrapperCDist, 'KL': WrapperKLDiv}
 
@@ -12,9 +8,4 @@ DISTRIBUTION_FUNCTION_FACTORY = {
     'beta': torch.distributions.beta.Beta,
     'uniform': torch.distributions.uniform.Uniform,
     'bernoulli': torch.distributions.bernoulli.Bernoulli,
-}
-
-BACKGROUND_TENSOR_FACTORY = {
-    'constant': WrapperBackgroundTensor.constant_value_tensor,
-    'range': WrapperBackgroundTensor.range_tensor,
 }
