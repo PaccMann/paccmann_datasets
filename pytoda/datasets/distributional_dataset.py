cea17e10857082d53044119130d848a05e0edcfd
Nikita Janakarajan <NJA@zurich.ibm.com>
2020-12-08 13:00:07 +0100

    fix: doctsring updated to add more info on distribution and device sampling

--- pytoda/datasets/distributional_dataset.py
+++ pytoda/datasets/distributional_dataset.py
@@ -2,24 +2,31 @@ from dataclasses import dataclass
 from typing import Optional, Tuple, Union
 
 import torch
-from torch.distributions.distribution import Distribution
-from torch.random import fork_rng
 from torch.utils.data import Dataset
+from torch.random import fork_rng
 
 from pytoda.types import Any, Tensor
 
 
 @dataclass
-class StochasticItems:
+class StochasticItems(Dataset):
     """Sample an item from a distribution on the fly on indexing.
 
     Args:
-        distribution (Distribution): the distribution to sample from.
-        shape (torch.Size): the desired shape of each item.
-        device (torch.device): device to send the tensor to.
+        distribution (torch.distributions.distribution.Distribution): An instance
+            of the torch distribution class to sample from. For example, for
+            loc = torch.tensor(0.0,device=device), and scale=torch.tensor(1.0,device=device),
+            torch.distributions.normal.Normal(loc,scale), so that
+            calling .sample() would return an item from this distribution on the specified device.
+            NOTE: The arguments to the distribution should be tensors on the desired device.
+            This ensure that samples are generated on this device and helps in avoiding
+            an overhead in sending each sampled item to device.
+        shape (torch.Size): The desired shape of each item.
+        device (torch.device): Device to send the tensor to. If the tensor is already
+            on device, then the .to() method returns self (no-ops).
     """
 
-    distribution: Distribution
+    distribution: torch.distributions.distribution.Distribution
     shape: Union[torch.Size, Tuple[int]]
     device: torch.device
 
@@ -32,6 +39,7 @@ class StochasticItems:
         Returns:
             Tensor: sampled from distribution with given shape.
         """
+
         return self.distribution.sample(self.shape).to(self.device)
 
 
@@ -42,7 +50,7 @@ class DistributionalDataset(Dataset):
         self,
         dataset_size: int,
         item_shape: Tuple[int],
-        distribution_function: Distribution,
+        distribution_function: torch.distributions.distribution.Distribution,
         seed: Optional[int] = None,
         device: torch.device = (
             torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -55,8 +63,16 @@ class DistributionalDataset(Dataset):
             item_shape (Tuple[int]): The shape of each item tensor returned on
                 indexing the dataset. For example for 2D items with timeseries of
                 3 timesteps and 5 features: (3, 5)
-            distribution_function (Distribution): The function from which individual
-                data items are to be sampled.
+            distribution_function (torch.distributions.distribution.Distribution):
+                An instance of the distribution class from which individual data
+                items can be sampled by calling the .sample() method.
+                This can either be an object that is directly initialised using a method
+                from torch.distributions, such as, torch.distributions.normal.Normal(loc=0.0,scale=1.0),
+                or from a factory using a keyword, for example,
+                DISTRIBUTION_FUNCTION_FACTORY['normal](loc=0.0, scale=1.0)
+                is a valid argument since the factory (found in utils.factories.py)
+                initialises the distribution class object based on a string keyword
+                and passes the relevant arguments to that object.
             seed (Optional[int]): If passed, all items are generated once with
                 this seed (using a local RNG only). Defaults to None, where
                 individual items are generated when the DistributionalDataset is
@@ -82,13 +98,16 @@ class DistributionalDataset(Dataset):
                 torch.cuda.manual_seed_all(seed)
 
                 self.datasource = self.data_sampler.sample((dataset_size, *item_shape))
-            # copy data to device
+
             self.datasource = self.datasource.to(device)
+
         else:
             # get sampled item on indexing
             self.datasource = StochasticItems(
-                self.data_sampler, self.item_shape, device
+                self.data_sampler, self.item_shape, self.device
             )
+
+        # copy data to device
 
     def __len__(self) -> int:
         """Gets length of dataset.
