9cbcf9e4f645339769d980c7b207799eeafdde50
Jannis Born <jab@zurich.ibm.com>
2020-12-24 10:40:08 +0100

    fix: pin selfies version also if pytoda is used as dep

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     license='MIT',
     install_requires=[
         'numpy', 'scikit-learn', 'pandas', 'torch>=1.0.0', 'diskcache', 'dill',
-        'selfies', 'upfp', 'SmilesPE>=0.0.3'
+        'selfies==0.2.4', 'upfp', 'SmilesPE>=0.0.3'
     ],
     classifiers=[
         'Intended Audience :: Developers',
