040f53fa5d30f8dbc4b04467e4a997f076f91428
Jannis Born <jab@zurich.ibm.com>
2021-03-18 17:07:32 +0100

    feat: DrugSensitivityConcentrationDataset (with test)

--- pytoda/datasets/__init__.py
+++ pytoda/datasets/__init__.py
@@ -3,6 +3,9 @@ from .base_dataset import KeyDataset, DatasetDelegator, ConcatKeyDataset  # noqa
 from .smiles_dataset import SMILESDataset, SMILESTokenizerDataset  # noqa
 from .gene_expression_dataset import GeneExpressionDataset  # noqa
 from .drug_sensitivity_dataset import DrugSensitivityDataset  # noqa
+from .drug_sensitivity_concentration_dataset import (  # noqa
+    DrugSensitivityConcentrationDataset,
+)
 from .annotated_dataset import AnnotatedDataset  # noqa
 from .protein_sequence_dataset import ProteinSequenceDataset  # noqa
 from .polymer_dataset import PolymerTokenizerDataset  # noqa
@@ -11,9 +14,9 @@ from .protein_protein_interaction_dataset import (  # noqa
 )
 from .drug_affinity_dataset import DrugAffinityDataset  # noqa
 from .utils import indexed, keyed  # noqa
-from .set_matching_dataset import (
+from .set_matching_dataset import (  # noqa
     SetMatchingDataset,
     PairedSetMatchingDataset,
     PermutedSetMatchingDataset,
-)  # noqa
+)
 from .distributional_dataset import DistributionalDataset  # noqa
