84cbc7f8a2ed23585188adaff83cd27d23def5da
Jannis Born <jab@zurich.ibm.com>
2021-03-26 10:11:44 +0100

    CI: Add conda installation to docs generation

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,26 @@
 #
 import os
 import sys
+import codecs
 
 sys.path.insert(0, os.path.abspath(".."))
+
+# -- Helper functions -----------------------------------------------------
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
 
 
 # -- Project information -----------------------------------------------------
@@ -21,6 +39,8 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "pytoda"
 copyright = "IBM Corp. 2021"
 author = "IBM Research"
+version = get_version('../pytoda/__init__.py')
+
 
 # -- Generate API (auto) documentation ------------------------------------------------
 
