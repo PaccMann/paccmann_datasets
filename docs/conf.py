9f61f1d7365cf7a97de3843b7f568a6d661694aa
Jannis Born <jab@zurich.ibm.com>
2021-03-26 10:37:42 +0100

    CI: gh pages deployment

--- docs/conf.py
+++ docs/conf.py
@@ -6,13 +6,14 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import codecs
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
-import codecs
 
 sys.path.insert(0, os.path.abspath(".."))
 
