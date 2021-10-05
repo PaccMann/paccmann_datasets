9df1f73129d51a2a30fc80e572884500411d8823
Jannis Born <jab@zurich.ibm.com>
2021-10-05 13:40:40 +0200

    wip: simplified smiles tokenizer package access

--- pytoda/smiles/__init__.py
+++ pytoda/smiles/__init__.py
@@ -1,1 +1,9 @@
+import json
+import os
+
 from .smiles_language import SELFIESLanguage, SMILESLanguage, SMILESTokenizer  # noqa
+
+with open(
+    os.path.join(os.path.dirname(__file__), 'metadata', 'tokenizer', 'vocab.json'), 'r'
+) as f:
+    vocab = json.load(f)
