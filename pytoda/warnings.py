9f7fffa9c2bdd0d87a3b24fec6dfdb5053eeaa48
Yoel Shoshan <yoels@il.ibm.com>
2022-05-13 15:59:12 +0300

    added throw_exception arg to device_warning

--- pytoda/warnings.py
+++ pytoda/warnings.py
@@ -1,11 +1,13 @@
 from .types import Any
 from warnings import warn
 
-def device_warning(device: Any) -> str:
+def device_warning(device: Any, throw_exception=True) -> str:
     if device is None:
         msg = (
             f"Found deprecated argument: device={device}. Device value will be ignored, "
             "all devices will be set to `cpu`."
         )
+    if throw_exception:
+        raise msg
     warn(msg)
     return msg
