ab20ab3da849390dc293df529c3bd05e12d88564
Yoel Shoshan <yoels@il.ibm.com>
2022-05-13 16:01:26 +0300

    a bit more info in the message

--- pytoda/warnings.py
+++ pytoda/warnings.py
@@ -5,7 +5,7 @@ def device_warning(device: Any, throw_exception=True) -> str:
     if device is None:
         msg = (
             f"Found deprecated argument: device={device}. Device value will be ignored, "
-            "all devices will be set to `cpu`."
+            "all devices will be set to `cpu`. If you are using cuda devices, also remember to transfer tensors to the gpu before using a model __call__"
         )
     if throw_exception:
         raise msg
