245c9b56bce2557b23004fa4101fcb5afea9d69a
Jannis Born <jab@zurich.ibm.com>
2022-06-20 14:23:58 +0200

    fix: alignment data now available as pytoda.proteins.kinase_as_alignment

--- pytoda/proteins/__init__.py
+++ pytoda/proteins/__init__.py
@@ -1,3 +1,11 @@
+import pandas as pd
+from importlib_resources import as_file, files
+
 from .protein_feature_language import ProteinFeatureLanguage  # noqa
 from .protein_language import ProteinLanguage  # noqa
 from .utils import aas_to_smiles  # noqa
+
+with as_file(
+    files('pytoda.proteins.metadata').joinpath('kinase_activesite_alignment.smi')
+) as alignment_filepath:
+    kinase_as_alignment = pd.read_csv(alignment_filepath, sep='\t')
