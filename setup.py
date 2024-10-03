c73d3fbf910a2347dd91b34d00da6534d7054d6a
Jannis Born <jab@zurich.ibm.com>
2024-10-03 17:30:15 +0200

    ci: expand tests to python 3.7-3.11

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 """Package installer."""
+
 import codecs
 import os
 
@@ -58,7 +59,10 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
